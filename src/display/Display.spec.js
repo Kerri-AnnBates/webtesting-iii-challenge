d78397159b5df6dffb1b56d25a8cc69a1da2788d
Kerri-Ann <kaybee1jam@yahoo.com>
2019-11-11 15:37:47 -0500

    created snapshot of Display component to ensure that gate default is open

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+import Display from './Display';
+
+describe('<Display />', () => {
+    test('It should match snapshot', () => {
+        const display = render('<Display />')
+        expect(display).toMatchSnapshot();
+    });
+});
