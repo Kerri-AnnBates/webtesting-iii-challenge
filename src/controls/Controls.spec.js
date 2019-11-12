836005aac8bdd6077d84c88016f16aaae65f5ad4
Kerri-Ann <kaybee1jam@yahoo.com>
2019-11-12 18:09:56 -0500

    added some tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,4 +1,35 @@
 // Test away!
-// Test away!
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import Controls from '../controls/Controls';
+import Display from '../display/Display';
+
+describe('<Controls />', () => {
+    // buttons' text changes to reflect the state the door will be in if clicked
+    test('close button clicked, open text shows', () => {
+        const toggleClosedMock = jest.fn();
+        const { getByText, findByText } = render(
+            <Controls toggleClosed={toggleClosedMock} locked={false} />
+        );
+        fireEvent.click(getByText(/close gate/i)); //1st button click
+        expect(toggleClosedMock).toHaveBeenCalled();
+        expect(findByText(/open gate/));
+    });
+
+    test('close button clicked, open text shows', async () => {
+        const toggleClosedMock = jest.fn();
+        const { findByText } = render(
+            <Controls toggleClosed={toggleClosedMock} closed={true} />
+        );
+        fireEvent.click(await findByText(/open gate/i));
+        expect(toggleClosedMock).toHaveBeenCalled();
+        expect(findByText(/close gate/));
+    });
+
+    test('renders the display', () => {
+        render(<Display />);
+    })
+
+    // the closed toggle button is disabled if the gate is locked
+    // the locked toggle button is disabled if the gate is open
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,42 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Controls from '../controls/Controls';
+import Display from '../display/Display';
+import Dashboard from '../dashboard/Dashboard';
+
+describe('<Dashboard />', () => {
+    test('renders Display component', ()=> {
+       render(<Display 
+           locked={false}
+           closed={false}
+       />);
+    })
+    test('renders Controls component', ()=> {
+       
+        const toggleLocked = jest.fn();
+        const toggleClosed = jest.fn();
+
+        render(<Controls 
+            locked={false}
+            closed={false}
+            toggleLocked={toggleLocked}
+            toggleClosed={toggleClosed}
+        />);
+
+    })
+
+    // defaults to unlocked and open
+    test('default render is unlocked and open', () => {
+        const {getByText} = render(<Dashboard />);
+        expect(getByText(/unlocked/i));
+        expect(getByText(/open/i));
+    });
+
+    // cannot be closed or opened if it is locked
+    test('cannot be closed or opened if it is locked', () => {
+        const toggleLockMock = jest.fn();
+        render (<Controls toggleLocked={toggleLockMock} locked={false} closed={false} />)
+        expect(toggleLockMock).not.toHaveBeenCalled();
+    })
+})
